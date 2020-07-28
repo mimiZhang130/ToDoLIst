0abf3ad0cec0e2c2df5ece10d5fd929bf8df582a
Mimi Zhang <mimi02420@gmail.com>
2020-07-28 10:08:53 -0400

    Initial Commit

---
+++ ToDoLIst/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoLIst
+//
+//  Created by Mimi Zhang on 7/28/20.
+//  Copyright © 2020 Mimi Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
